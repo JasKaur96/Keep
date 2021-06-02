8d6ecbb056a10e31c90bbfb2470a91c99a645d82
JasKaur96 <jaspreetflora7@gmail.com>
2021-06-02 09:12:18 +0530

    Added: Color Header

---
+++ src/Components/Google-Header/GoogleHeader.jsx
@@ -0,0 +1,19 @@
+import './googleHeader.css';
+
+const GoogleHeader = () => {
+    return(
+        <div className="header">  
+            <div className="google-main">
+                    <span style={{"color":"blue"}}>G</span>
+                    <span  style={{"color":"red"}}>o</span>
+                    <span  style={{"color":"yellow"}}>o</span>
+                    <span  style={{"color":"blue"}}>g</span>
+                    <span  style={{"color":"green"}}>l</span>
+                    <span  style={{"color":"red"}}>e</span>      
+            </div> 
+        </div>
+    )
+   
+}
+
+export default GoogleHeader
